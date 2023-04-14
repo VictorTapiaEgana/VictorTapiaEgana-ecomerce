fd51995cb34edd12d5a2a8421b097260c4fc05bf
Victor <victortapiaegana@gmail.com>
2023-04-14 13:20:44 -0400

    se elimino clase cabecera del App.js y agregaron clases de Bootstrap al NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,30 +1,26 @@
-import "./NavBar.css";
 import Logo from "../../assets/logo/Logo2_nbg.png";
 import CartWidget from "../CartWidget/CartWidget";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
-   
-      <nav className="navbar bg-body-tertiary">
-         <div className="container-fluid"> 
-        
-          <img className="imgnombreEmpresa" src={Logo} alt="Nombre Empresa" />
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      
+      <div className="container-fluid">        
+            <img className="imgnombreEmpresa" src={Logo} alt="Nombre Empresa" />        
+            
+            <div className="d-flex">
+              <a className="navbar-brand" href="#">Computacion</a>
+              <a className="navbar-brand" href="#">Sofas</a>
+              <a className="navbar-brand" href="#">Deportes</a>   
 
-          <a className="navbar-brand" href="#">
-            Computacion
-          </a>
-          <a className="navbar-brand" href="#">
-            Sofas
-          </a>
-          <a className="navbar-brand" href="#">
-            Deportes
-          </a>   
+              <CartWidget className="flex-grow-1" />
+            </div>
 
-            <CartWidget />          
+      </div>
 
-        </div>
-      </nav>
-   
+    </nav>
   );
 };
+
 export default NavBar;
